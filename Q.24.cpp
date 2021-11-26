e796b12afb5cb52cdc5c655f4f51dd8ed9d90da1
Yogesh Narendra Tiwari <92584567+yogeshnarendratiwari@users.noreply.github.com>
2021-11-26 14:05:43 +0530

    26-11-2021

--- Q.24.cpp
+++ Q.24.cpp
@@ -6,3 +6,11 @@
 
 // About OOPS
 // set of obejcts and objects contains functions & data
+
+// Properties
+// 1.Abstraction
+// 2.Encapsulation --> Data hiding
+// 3.Inheritance 
+// 4.Polymorphism
+
+// class --> blueprint of object
